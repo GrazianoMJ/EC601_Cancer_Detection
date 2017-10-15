6237adc2729b6028510c917d0a4e34d49d286ed6
GrazianoMJ <GrazianoMJ@users.noreply.github.com>
2017-10-15 17:29:33 -0400

    Training_Text read_csv() attempt and data_preprocess.py clean-up * Attempted to use read_csv() to extract text data; too large for the DataFrame. Will need to find an alternative * Cleaned out all unused code from previous csv_decoding function and main() commented out code to test this function.

--- data_preprocess.py
+++ data_preprocess.py
@@ -1,23 +1,10 @@
 import pandas
 
-def csv_decoder(filename):
-
-    """
-    csv_decoder docstring text
-    """
-
-    output_list = []
-    with open(filename,"r") as file:
-        for line in file:
-            output_list.append(line.rstrip("\n").split(","))
-    return output_list
-
 def main():
-#    variants = csv_decoder("training_variants.txt")
-#    for row in variants:
-#        print(row)
-    new_variants = pandas.read_csv("./training_variants.txt")
-    with pandas.option_context('display.max_rows', None, 'display.max_columns', 5):
+    new_variants = pandas.read_csv("~/Desktop/training_variants.txt")
+    with pandas.option_context('display.max_rows', None, 
+                               'display.max_columns', None
+                              ):
         print(new_variants)
     
 if __name__=="__main__":
