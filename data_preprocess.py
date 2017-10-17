35c4e7e383407f143772386c8e57355c4587cc8f
GrazianoMJ <GrazianoMJ@users.noreply.github.com>
2017-10-17 14:12:50 -0400

    Create Text Documents * Created code that uses os library to extract text files into individual text files. * Each text file labeled with text####.txt where #### is a zero padded number corresponding to the reference for the file.

--- data_preprocess.py
+++ data_preprocess.py
@@ -1,11 +1,24 @@
+import sys
+import os
 import pandas
 
 def main():
-    new_variants = pandas.read_csv("~/Desktop/training_variants.txt")
-    with pandas.option_context('display.max_rows', None, 
-                               'display.max_columns', None
-                              ):
-        print(new_variants)
+    variants = pandas.read_csv("~/Desktop/training_variants.txt")
+#    with pandas.option_context('display.max_rows', None, 
+#                               'display.max_columns', None
+#                              ):
+#        print(variants)
+    with open("/home/grazianomj/Desktop/"+"training_text.txt", "r") as file:
+        for line in file:
+            if line == "ID,Text\n":
+                continue
+            else:
+                temp = line.split(r"||")
+                with open("/home/grazianomj/Desktop/training_docs/" +
+                          "text{:04d}".format(int(temp[0])) +
+                          ".txt", "w"
+                ) as target:
+                    target.write(temp[1])
     
 if __name__=="__main__":
     main()
