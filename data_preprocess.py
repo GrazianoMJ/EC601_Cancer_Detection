eb3f00f38ef22e464cb84e12bceb3fd1948eb009
GrazianoMJ <GrazianoMJ@users.noreply.github.com>
2017-10-18 09:53:24 -0400

    Text Document into Pandas Data Frame Success * Created lines of code that pulled in the text from training_text and added it to the DataFrame. * Updated results.txt in order to view if the import was successful.

--- data_preprocess.py
+++ data_preprocess.py
@@ -1,24 +1,30 @@
 import sys
 import os
+import csv
 import pandas
 
 def main():
     variants = pandas.read_csv("~/Desktop/training_variants.txt")
-#    with pandas.option_context('display.max_rows', None, 
-#                               'display.max_columns', None
-#                              ):
+    new_text = pandas.read_csv("~/Desktop/training_text.txt",
+                               sep="\\|\\|",
+                               engine="python"
+    )
+    with pandas.option_context('display.max_rows', None, 
+                               'display.max_columns', None
+                              ):
+        print(new_text)
 #        print(variants)
-    with open("/home/grazianomj/Desktop/"+"training_text.txt", "r") as file:
-        for line in file:
-            if line == "ID,Text\n":
-                continue
-            else:
-                temp = line.split(r"||")
-                with open("/home/grazianomj/Desktop/training_docs/" +
-                          "text{:04d}".format(int(temp[0])) +
-                          ".txt", "w"
-                ) as target:
-                    target.write(temp[1])
+#    with open("/home/grazianomj/Desktop/"+"training_text.txt", "r") as file:
+#        for line in file:
+#            if line == "ID,Text\n":
+#                continue
+#            else:
+#                temp = line.split(r"||")
+#                with open("/home/grazianomj/Desktop/training_docs/" +
+#                          "text{:04d}".format(int(temp[0])) +
+#                          ".txt", "w"
+#                ) as target:
+#                    target.write(temp[1])
     
 if __name__=="__main__":
     main()
