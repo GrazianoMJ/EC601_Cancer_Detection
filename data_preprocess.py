79b9734079efe6c11d6de5f713df56be5e48ac78
GrazianoMJ <GrazianoMJ@users.noreply.github.com>
2017-11-03 15:35:46 -0400

    Preprocess Update: Added code to extract and save Test Data * Updated code to utilize the extract_data function created in the collation.py program. * Added global variables to allow for work to be performed independently on Training and Testing data. * Forced the code to only focus on testing since training had been completed. This will need to be adjusted in the code to make it work for both sets of data.

--- data_preprocess.py
+++ data_preprocess.py
@@ -4,7 +4,8 @@ from nltk import word_tokenize
 from nltk.corpus import stopwords
 
 DIRECT = "/home/grazianomj/Desktop/"
-VAR_HEADS = ["ID", "Gene", "Variation", "Class"]
+TARGET = ["Training/", "Test/"]
+FILE_PREFIX = ["training_", "test_"]
 TXT_HEADS = ["ID", "Text"]
 STOPS = set(stopwords.words("english"))
 
@@ -15,29 +16,38 @@ def simple_text(raw_text):
     clean_text = " ".join(no_stops)
     return clean_text
 
-def main():
-    variants = pandas.read_csv(DIRECT+"training_variants.txt")
-    text = pandas.read_csv(DIRECT+"training_text.txt",
-                               header=0,
-                               sep="\\|\\|",
+def extract_data(folder, prefix, designation, suffix):
+    file_name = DIRECT+folder+prefix+designation+suffix
+    if suffix == ".csv":
+        return pandas.read_csv(file_name)
+    else:
+        return pandas.read_csv(file_name, header=0, sep="\\|\\|",
                                engine="python"
-    )
+        )
 
-    text_size = text[TXT_HEADS[1]].size
-    clean_text_docs = []
-    for row in range(0, text_size):
-        if (row+1)%50 == 0 or not row:
-            print("Review {} of {} in progress".format(row+1, text_size))
-        clean_text_docs.append(simple_text(text.get_value(row, TXT_HEADS[1])))
-    with open(DIRECT+"training_text_mod.txt", "w") as file:
-        file.write(TXT_HEADS[0]+r"||"+TXT_HEADS[1]+"\n")
+def main():
+    for i in range(1,2):
+        file_name1 = DIRECT+TARGET[i]+FILE_PREFIX[i]+"text.txt"
+        file_name2 = DIRECT+TARGET[i]+FILE_PREFIX[i]+"_mod_text.txt"
+        txt_df = pandas.read_csv(file_name1, header=0, sep="\\|\\|",
+                                 engine="python"
+        )
+    
+        text_size = txt_df[TXT_HEADS[1]].size
+        clean_text_docs = []
         for row in range(0, text_size):
-            text2write = (str(text.get_value(row,TXT_HEADS[0])) + 
-                          r"||" +
-                          clean_text_docs[row] +
-                          "\n"
-            )
-            file.write(text2write)
+            if (row+1)%50 == 0 or not row:
+                print("Review {} of {} in progress".format(row+1, text_size))
+            clean_text_docs.append(simple_text(txt_df.get_value(row, TXT_HEADS[1])))
+        with open(file_name2, "w") as file:
+            file.write(TXT_HEADS[0]+r"||"+TXT_HEADS[1]+"\n")
+            for row in range(0, text_size):
+                text2write = (str(txt_df.get_value(row,TXT_HEADS[0])) + 
+                              r"||" +
+                              clean_text_docs[row] +
+                              "\n"
+                )
+                file.write(text2write)
 
 if __name__=="__main__":
     main()
