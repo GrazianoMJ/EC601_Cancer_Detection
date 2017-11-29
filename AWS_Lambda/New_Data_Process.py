0893f3c647a015e2e3213fcd95655cc8f03c5bad
GrazianoMJ <GrazianoMJ@users.noreply.github.com>
2017-11-29 12:33:15 -0500

    Pulled in New_Data_Process.py code from the Lambda * Retrieved the script that I have from Lambda and put that into our Github repository. * Added docstring explaining what the code does.

--- AWS_Lambda/New_Data_Process.py
+++ AWS_Lambda/New_Data_Process.py
@@ -1,15 +1,33 @@
-import zipfile 
+""" New_Data_Process.py - AWS Lambda Code
 
-MAIN_DIR = "/home/grazianomj/Desktop/"
-FILE_DIR = "training_docs/"
+    This program is designed to receive an event from Amazon AWS S3
+    service and then perform the following tasks:
 
-def main():
-    with zipfile.ZipFile(MAIN_DIR+"test_dir.zip", "w") as zf:
-        for i in range(0, 50):
-            text_file = "text{0:04d}.txt".format(i)
-            zf.write(MAIN_DIR+FILE_DIR+text_file, 
-                     arcname=text_file
-            )
+    * Pass important information about the S3 bucket and file to the
+      Amazon AWS SQS queue.
+    * Pass commands to the EC2 Instance Service Manager to execute our 
+      learning model.
+"""
 
-if __name__ == '__main__':
-    main()
+import boto3
+import json
+
+def handler(event, context):
+    ssm = boto3.client("ssm")
+    sqs = boto3.resource("sqs")
+    queue = sqs.get_queue_by_name(QueueName="cancerdetect.fifo")
+    response = queue.send_message(MessageBody=json.dumps(event),
+                                  MessageGroupId="Test"
+    )
+    
+    ssmCommand = ssm.send_command(
+        InstanceIds = ["i-0209de525bf9cc235"],
+        DocumentName = "AWS-RunShellScript",
+        TimeoutSeconds = 240,
+        Comment = "CancerDetectExample",
+        Parameters = {
+            "commands":["export PATH='/home/ubuntu/anaconda3/bin:$PATH'",
+                        "cd /home/ubuntu/", "python test.py"
+            ]
+        }
+    )
